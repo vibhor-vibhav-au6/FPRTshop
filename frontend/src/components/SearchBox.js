4a4434854c52508e0d708af3ad8c7a66246200ef
vibhor <vibhorsmiki@gmail.com>
2020-10-22 00:58:38 +0530

    rating+search component

---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`)
+    } else {
+      history.push('/')
+    }
+  }
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+      <Form.Control
+        type='text'
+        name='q'
+        onChange={(e) => setKeyword(e.target.value)}
+        placeholder='find products'
+        className='mr-sm-1 ml-sm-5'
+
+      ></Form.Control>
+      <Button type='submit' variant='dark' >
+      <i className="fa fa-search" aria-hidden="true"></i> Go
+      </Button>
+    </Form>
+  )
+}
+
+export default SearchBox
