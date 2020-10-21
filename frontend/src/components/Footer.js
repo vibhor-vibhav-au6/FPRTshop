17e8fe552294bcb24aef7074c587c47dc625ac70
vibhor <vibhorsmiki@gmail.com>
2020-10-21 17:35:25 +0530

    React setup

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>&copy; FPRTshop by Vibhor @ www.vibhorvibhav.com</Col>
+        </Row>
+
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
